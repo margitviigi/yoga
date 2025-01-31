6fac80bf63fd4b2a1302e1637832e84b6befcdf4
Margit Viigi <margit.viigi@voco.ee>
2025-01-31 11:42:03 +0200

    As a user i can see all yoga articles. See #1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const app = require('./utils/app')
 const db = require('./utils/db')
+const articleRoutes = require('./routes/articles')
+
+app.use('/', articleRoutes)
 
 app.listen(3012, () => {
     console.log('Web server is connected.')
